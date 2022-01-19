65bd194c051cb2dfffc0fb88f8d2930a0d718397
Adeolu Oyinlola <adeoluoyin@gmail.com>
2022-01-18 20:53:13 -0800

    set up

---
+++ src/TicTacToe/TicTacToe.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './TicTacToe.css';
+
+const TicTacToe = () => {
+	const [turn, setTurn] = useState('x');
+	const [cells, setCells] = useState(Array(9).fill(''));
+	
+	const handleClick = (num) => {
+		if (cells[num] !== '') {
+			alert('already clicked');
+			return;
+		}
+
+		let squares = [...cells];
+
+		if (turn === 'x') {
+			squares[num] = 'x';
+			setTurn('o');
+		} else {
+			squares[num] = 'o';
+			setTurn('x');
+		}
+
+		checkForWinner(squares);
+		setCells(squares);
+	};
+
+	const Cell = ({ num }) => {
+		return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+	};
+
+	return (
+		<div className='container'>
+			<table>
+				Turn: {turn}
+				<tbody>
+					<tr>
+						<Cell num={0} />
+						<Cell num={1} />
+						<Cell num={2} />
+					</tr>
+					<tr>
+						<Cell num={3} />
+						<Cell num={4} />
+						<Cell num={5} />
+					</tr>
+					<tr>
+						<Cell num={6} />
+						<Cell num={7} />
+						<Cell num={8} />
+					</tr>
+				</tbody>
+			</table>
+			
+		</div>
+	);
+};
+
+export default TicTacToe;
